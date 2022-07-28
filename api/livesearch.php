152d5c69c5d376683911e00b5c38168ab64616a0
Thomas Rowlands <thomas.s.rowlands@gmail.com>
2022-07-28 15:25:24 +0100

    Fixed autocomplete cypher query speed.

--- api/livesearch.php
+++ api/livesearch.php
@@ -8,13 +8,14 @@ if (isset($_GET["entry"]) && isset($_GET["ontology"])) {
     $cmd = "";
     $ont = strtolower($_GET["ontology"]);
     $entry = strtolower($_GET["entry"]);
-    $cmd = "MATCH (n)
+    $cmd = "MATCH (n)-[:hasGWASResult|:hasExperimentResult]-()
     WHERE n.ontology in [" . ($ont == "mp" ? "'mp'" : "'mesh', 'hpo'") . "] AND  toLower(n.FSN) STARTS WITH {entry} AND n.isObsolete = \"false\"
     WITH n
     OPTIONAL MATCH (n)<-[:HAS_SYNONYM]-(m)
     WITH n, m
     RETURN DISTINCT n.FSN AS FSN, n.originalType AS type, n.ontology AS ontology, COALESCE(n.id, m.id) AS id, COALESCE(m.FSN, n.FSN) AS Term, COALESCE(m.id, n.id) AS TermID
     ORDER BY FSN
+    LIMIT 6
     ;";
     $neo = new Neo_Connection();
     $result = $neo->execute($cmd, ['entry' => $entry]);
