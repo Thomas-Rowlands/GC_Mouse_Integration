371c0e16b76ea892244b06a6ddf81f37efe7b2c5
Thomas Rowlands <thomas.s.rowlands@gmail.com>
2022-08-19 13:20:14 +0100

    Commented out error reporting flags.

--- api/tree.php
+++ api/tree.php
@@ -1,8 +1,8 @@
 <?php
     include_once 'database.php';
     include_once 'utility.php';
-    error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//    error_reporting(E_ALL);
+//    ini_set('display_errors', '1');
 
     class OntologyTree {
 
